1a2ea7521e954a26d6963b9bbabdd08f1fa8a7c0
Diogo M <diogo_monteiro_1@hotmail.com>
2018-05-13 22:38:15 +0200

    rearrange identation + rename some folders

---
+++ README (Case Conflict).md
@@ -0,0 +1,24 @@
+# Project Title
+
+Quadcopter control software for arduino platform.
+
+## Getting Started
+
+To do.
+
+### Prerequisites
+
+To do.
+
+## Authors
+
+* **Diogo M.
+
+## License
+
+MIT License
+
+## Acknowledgments
+
+N/A
+
--- xcopter_main/Xcopter_main.ino
+++ xcopter_main/Xcopter_main.ino
@@ -24,7 +24,7 @@ float phi_prev = 0.0,err_phi_prev = 0.0;
 float theta_prev = 0.0,err_theta_prev = 0.0;
 /*motors*/
 int m0_spd = 0;
-int m1_spd = 0;	
+int m1_spd = 0; 
 int m2_spd = 0;
 int m3_spd = 0;
 
@@ -53,145 +53,145 @@ float bd = -kd/(kd+N*T_LOOP);
 
 
 void setup() {
-	// put your setup code here, to run once:
-        Serial.begin(115200);
-	leds_init();
-        led(G_LED,HIGH,0);
-        //calibrate_motors();
-        //arm_motors();
-        led(R_LED,HIGH,2); //blink*/
-        calibrate_sensors();
-	led(R_LED,HIGH,2); //blink
-	rx_init();
-        led(G_LED,LOW,0);
-	timer_control = millis();
-        timer_rx = millis();
-        timer_print = millis();
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    leds_init();
+    led(G_LED,HIGH,0);
+    //calibrate_motors();
+    //arm_motors();
+    led(R_LED,HIGH,2); //blink*/
+    calibrate_sensors();
+    led(R_LED,HIGH,2); //blink
+    rx_init();
+    led(G_LED,LOW,0);
+    timer_control = millis();
+    timer_rx = millis();
+    timer_print = millis();
 
 }
 
 void loop() {
-	// put your main code here, to run repeatedly:
-        
-        float dt_loop = (millis()-timer_control)/1000.0;
-        
-              
-        if( dt_loop >= T_LOOP)
-        {
-          timer_control = millis();
-          update_ControlVariables();
+// put your main code here, to run repeatedly:
+    
+    float dt_loop = (millis()-timer_control)/1000.0;
+    
           
-          if(RxLoss_flag == false){
-            ControlPID();
-          }
-          else
-          {
-            set_motors(ZeromSpeed);
-                        
-           }
-          
-         }
-         
-        if((millis()-timer_rx)/1000.0 >= T_RX)
-        {
-          timer_rx = millis();
-          update_ControlReferences();
-          RxLoss_flag = CheckRxLoss();
-          
-        }
-        
-        
-        //Tuning
-        if(Serial.available() > 0){
-          while(Serial.available()>0){Serial.read();delay(25);} //clean buffer
-          led(G_LED,HIGH,0);
-          set_motors(ZeromSpeed);delay(100);
-          Serial.println("What to adjust: P(1) - I(2) - D(3) ");while(Serial.available()<=0){delay(25);}
-          int rx_byte = Serial.read()-48;delay(25);Serial.println(rx_byte);
-          while(Serial.available()>0){Serial.read();delay(25);} //clean buffer
-          int i = 0;
-          switch (rx_byte) {
-            case 1:
-                  Serial.print("P gain: ");Serial.println(kp,6);/*AQUI*/
-                  Serial.println("new gain?");while(Serial.available()<=0){delay(25);}
-                  rx_byte = Serial.read();
-                  kp = 0.0;/*AQUI*/
-                  while( rx_byte != '\n')
-                  {
-                     if( rx_byte >= '0' && rx_byte <= '9'){
-                      
-                      kp += (float)(rx_byte-48)/pow(10,i);/*AQUI*/
-                      i++;
-                     }
-                      rx_byte = Serial.read();delay(25);
-                      
-                   }
-                   Serial.println(kp,6);/*AQUI*/
-                   break;
-             case 2:
-                  Serial.print("I gain: ");Serial.println(ki,6);/*AQUI*/
-                  Serial.println("new gain?");while(Serial.available()<=0){delay(25);}
-                  rx_byte = Serial.read();
-                  ki = 0.0;/*AQUI*/
-                  while( rx_byte != '\n')
-                  {
-                     if( rx_byte >= '0' && rx_byte <= '9'){
-                      
-                      ki += (float)(rx_byte-48)/pow(10,i);/*AQUI*/
-                      i++;
-                     }
-                      rx_byte = Serial.read();delay(25);
-                      
-                   }
-                   Serial.println(ki,6);/*AQUI*/
-                   break;
-             case 3:
-                  Serial.print("D gain: ");Serial.println(kd,6);/*AQUI*/
-                  Serial.println("new gain?");while(Serial.available()<=0){delay(25);}
-                  rx_byte = Serial.read();
-                  kd = 0.0;/*AQUI*/
-                  while( rx_byte != '\n')
-                  {
-                     if( rx_byte >= '0' && rx_byte <= '9'){
-                      
-                      kd += (float)(rx_byte-48)/pow(10,i);/*AQUI*/
-                      i++;
-                     }
-                      rx_byte = Serial.read();delay(25);
-                      
-                   }
-                   Serial.println(kd,6);/*AQUI*/
-                   break;
-              default:
-                Serial.println("not option");
-                break;
-           }
-           ai = (float)(ki*T_LOOP);
-           ad = (float)(kd/(kd+N*T_LOOP));
-           bd = (float)(-kd/(kd+N*T_LOOP));
-           I_roll = 0.0;
-           D_roll = 0.0;
-           I_pitch = 0.0;
-           D_pitch = 0.0;        
-           while(ref_lift > 950){led(R_LED,HIGH,2);}//blink
-           led(G_LED,LOW,0);
-           }
-        //proof_t = millis()-proof_t;
-        
-        /*if(proof_t > max_t)
-          max_t = proof_t;*/
-        
-        if(millis() - timer_print > 500){
-           Serial.print("m0: ");Serial.print(m0_spd);Serial.print(" ");
-           Serial.print("m1: ");Serial.print(m1_spd);Serial.print(" ");
-           Serial.print("m2: ");Serial.print(m2_spd);Serial.print(" ");
-           Serial.print("m3: ");Serial.print(m3_spd);Serial.print(" | ");
-           Serial.print("phi: ");Serial.print(phi);Serial.print(" ");
-           Serial.print("ref_phi: ");Serial.print(ref_phi);Serial.print(" | ");
-           Serial.print("theta: ");Serial.print(theta);Serial.print(" ");
-           Serial.print("ref_theta: ");Serial.print(ref_theta);Serial.println(" ");
-           //Serial.print("time: ");Serial.println(max_t);
-           timer_print = millis();
-         }
-	//led(G_LED,HIGH,1);
+    if( dt_loop >= T_LOOP)
+    {
+      timer_control = millis();
+      update_ControlVariables();
+      
+      if(RxLoss_flag == false){
+        ControlPID();
+      }
+      else
+      {
+        set_motors(ZeromSpeed);
+                    
+       }
+      
+     }
+     
+    if((millis()-timer_rx)/1000.0 >= T_RX)
+    {
+      timer_rx = millis();
+      update_ControlReferences();
+      RxLoss_flag = CheckRxLoss();
+      
+    }
+    
+    
+    //Tuning
+    if(Serial.available() > 0){
+      while(Serial.available()>0){Serial.read();delay(25);} //clean buffer
+      led(G_LED,HIGH,0);
+      set_motors(ZeromSpeed);delay(100);
+      Serial.println("What to adjust: P(1) - I(2) - D(3) ");while(Serial.available()<=0){delay(25);}
+      int rx_byte = Serial.read()-48;delay(25);Serial.println(rx_byte);
+      while(Serial.available()>0){Serial.read();delay(25);} //clean buffer
+      int i = 0;
+      switch (rx_byte) {
+        case 1:
+              Serial.print("P gain: ");Serial.println(kp,6);/*AQUI*/
+              Serial.println("new gain?");while(Serial.available()<=0){delay(25);}
+              rx_byte = Serial.read();
+              kp = 0.0;/*AQUI*/
+              while( rx_byte != '\n')
+              {
+                 if( rx_byte >= '0' && rx_byte <= '9'){
+                  
+                  kp += (float)(rx_byte-48)/pow(10,i);/*AQUI*/
+                  i++;
+                 }
+                  rx_byte = Serial.read();delay(25);
+                  
+               }
+               Serial.println(kp,6);/*AQUI*/
+               break;
+         case 2:
+              Serial.print("I gain: ");Serial.println(ki,6);/*AQUI*/
+              Serial.println("new gain?");while(Serial.available()<=0){delay(25);}
+              rx_byte = Serial.read();
+              ki = 0.0;/*AQUI*/
+              while( rx_byte != '\n')
+              {
+                 if( rx_byte >= '0' && rx_byte <= '9'){
+                  
+                  ki += (float)(rx_byte-48)/pow(10,i);/*AQUI*/
+                  i++;
+                 }
+                  rx_byte = Serial.read();delay(25);
+                  
+               }
+               Serial.println(ki,6);/*AQUI*/
+               break;
+         case 3:
+              Serial.print("D gain: ");Serial.println(kd,6);/*AQUI*/
+              Serial.println("new gain?");while(Serial.available()<=0){delay(25);}
+              rx_byte = Serial.read();
+              kd = 0.0;/*AQUI*/
+              while( rx_byte != '\n')
+              {
+                 if( rx_byte >= '0' && rx_byte <= '9'){
+                  
+                  kd += (float)(rx_byte-48)/pow(10,i);/*AQUI*/
+                  i++;
+                 }
+                  rx_byte = Serial.read();delay(25);
+                  
+               }
+               Serial.println(kd,6);/*AQUI*/
+               break;
+          default:
+            Serial.println("not option");
+            break;
+       }
+       ai = (float)(ki*T_LOOP);
+       ad = (float)(kd/(kd+N*T_LOOP));
+       bd = (float)(-kd/(kd+N*T_LOOP));
+       I_roll = 0.0;
+       D_roll = 0.0;
+       I_pitch = 0.0;
+       D_pitch = 0.0;        
+       while(ref_lift > 950){led(R_LED,HIGH,2);}//blink
+       led(G_LED,LOW,0);
+       }
+    //proof_t = millis()-proof_t;
+    
+    /*if(proof_t > max_t)
+      max_t = proof_t;*/
+    
+    if(millis() - timer_print > 500){
+       Serial.print("m0: ");Serial.print(m0_spd);Serial.print(" ");
+       Serial.print("m1: ");Serial.print(m1_spd);Serial.print(" ");
+       Serial.print("m2: ");Serial.print(m2_spd);Serial.print(" ");
+       Serial.print("m3: ");Serial.print(m3_spd);Serial.print(" | ");
+       Serial.print("phi: ");Serial.print(phi);Serial.print(" ");
+       Serial.print("ref_phi: ");Serial.print(ref_phi);Serial.print(" | ");
+       Serial.print("theta: ");Serial.print(theta);Serial.print(" ");
+       Serial.print("ref_theta: ");Serial.print(ref_theta);Serial.println(" ");
+       //Serial.print("time: ");Serial.println(max_t);
+       timer_print = millis();
+     }
+//led(G_LED,HIGH,1);
 }
